fbed15ac09bd15a1f177ba166f3eb22e57555c97
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-01-01 13:57:40 +0200

    first commit of queries

---
+++ queries.sql
@@ -0,0 +1,25 @@
+-- Active: 1735564289448@@127.0.0.1@3306@restaurant
+CREATE TABLE person(  
+    person_id smallint unsigned,
+    fname varchar(20),
+    lname varchar(20),
+    eye_color enum('br','bl','gr'),
+    birth_date date,
+    street varchar(30),
+    city varchar(20),
+    state varchar(20),
+    country varchar(20),
+    postal_code varchar(20),
+    constraint pk_person primary key (person_id)
+);
+
+create table favourite_food(
+    person_id smallint unsigned,
+    food varchar(20),
+    constraint pk_favorite_food primary key(person_id, food),
+    constraint fk_fav_food_person_id foreign key(person_id) references person (person_id)
+);   
+
+alter table favourite_food rename favorite_food;
+
+show tables;
